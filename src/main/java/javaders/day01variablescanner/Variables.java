a7bd873afd7912ebbb4f216c88356644b6a3f5fe
YusufCoban <ysfcbn49@gmail.com>
2023-04-27 20:24:39 +0300

    day1 variables example

---
+++ src/main/java/javaders/day01variablescanner/Variables.java
@@ -0,0 +1,44 @@
+package javaders.day01variablescanner;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        byte bytemin = Byte.MIN_VALUE;
+        byte bytemax = Byte.MAX_VALUE;
+
+        short shortminValue = Short.MIN_VALUE;
+        short shortmaxValue = Short.MAX_VALUE;
+
+        int intmindeger = Integer.MIN_VALUE;
+        int intmaxdeger = Integer.MAX_VALUE;
+
+        long longminimum = Long.MIN_VALUE;
+        long longmaximum = Long.MAX_VALUE;
+
+        double doublemin = Double.MIN_VALUE;
+        double doublemaax = Double.MAX_VALUE;
+
+        float floatmin = Float.MIN_VALUE;
+        float floatmax = Float.MAX_VALUE;
+
+        System.out.println("bytemax = " + bytemax);
+        System.out.println("shortmaxValue = " + shortmaxValue);
+        System.out.println("intmaxdeger = " + intmaxdeger);
+        System.out.println("longmaximum = " + longmaximum);
+        System.out.println("doublemaax = " + doublemaax);
+        System.out.println("floatmax = " + floatmax);
+
+        System.out.println("bytemin = " + bytemin);
+        System.out.println("shortminValue = " + shortminValue);
+        System.out.println("intmindeger = " + intmindeger);
+        System.out.println("longminimum = " + longminimum);
+        System.out.println("doublemin = " + doublemin);
+        System.out.println("floatmin = " + floatmin);
+
+
+
+    }
+
+
+
+}
