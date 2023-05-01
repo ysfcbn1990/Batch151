f5a7415e9e63922ea9cdea49f1ea672c44e99443
YusufCoban <ysfcbn49@gmail.com>
2023-05-02 01:07:24 +0300

    variable1

--- src/main/java/javaders/Exercise.java
+++ src/main/java/javaders/Exercise.java
@@ -1,29 +1,37 @@
 package javaders;
 
-public class Tekrar {
+public class Exercise {
     public static void main(String[] args) {
-
         //Ornek 1 : ogrenci ismi icin variable olusturup deger olarak Ali Can atayiniz
         String name="Ali Can";
+
         //Ornek 2 : char data typenda bir ismin ilk harfi olarak bir variable olusturunuz ve bir deger atayiniz
-        char firstletter='Y';
+        char firstLetter='Y';
+
         //Ornek 3 : boolean data typenda emekli misin icin bir variable olusturun ve false degerini atayınız
         boolean isRetired=false;
+
         //Ornek4 : byte data typenda ögrenci yası icin bir variable olusturun ve bir deger atayınız
         byte ageOfStudent=33;
+
         // Ornek 5 : Site nufusu icin bir variable olusturun ve bir deger atayınız
-        short populationOfcity=1500;
+        short populationOfsite=1560;
+
         //Ornek 6:Ulke nufusu icin bir variable olusturun ve bir deger atayınız
-        int populationOfCountry=86000000;
+        int countrypopulation = 56748888 ;
+
         //Ornek 7.Insan vucudundaki hücre sayısı icin variable olusturup deger atayınız
-        long hucreSayisi=65858545854558l;
+        long hucreSayisi=656668654236655l;
+
         //Ornek 8: Gömlek ve ayakkabı fiyatları icin 2 tane variable olusturup toplam fiyatı ekrana yazdırınız.
-        float priceshirt=123.77f;
-        float priceshoes=565.55f;
+        float shirtPrice = 12.99F;
+        float shoesPrice = 15.99f;
+
         //Ornek hücre agırlığı ve amipin agırlıgı icin double data typende variable olusturup farklarını ekrana yazdırınız
         double hucreAgirligi = 0.0000056 ;
         double amipAgirligi = 0.00000000000078 ;
-        //Örnek 1: şehir ismi için bir variable oluşturun ve bir değer atayıp onu ekrana yazdırın.
+
+
 
     }
 }
